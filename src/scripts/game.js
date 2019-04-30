301e4facc3f3d5c17cc5df838c0ed5ef3f3cf5b9
Jack Laird <jack.laird0@gmail.com>
2019-04-30 12:10:00 -0600

    Make currentRound reset after 4 rounds

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -8,12 +8,12 @@ class Game {
   
 
   roundAdvance() {
-    this.currentRound += 1;
+    this.currentRound === 4 ? this.currentRound = 1 : this.currentRound ++;
     this.usedLetters = [];
   }
 
   nextPlayer() {
-    this.currentPlayer === 3 ? this.currentPlayer = 1 : this.currentPlayer ++
+    this.currentPlayer === 3 ? this.currentPlayer = 1 : this.currentPlayer ++;
   }
 
   spinWheel(value) {
